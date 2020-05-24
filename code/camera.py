8a9daedf3cdc194a518c24c448e9562f35059202
daddabarba <davidebarbieri97@gmail.com>
2020-05-24 22:56:02 +0200

    Small refactoring

--- code/camera.py
+++ code/camera.py
@@ -5,28 +5,25 @@ def get_P(bottom_left : tuple, top_right : tuple, n : float, f : float):
     l, b = bottom_left
     r, t = top_right
 
-    return np.array(
-        [
-            [2*n/(r-l), 0,         (r+l)/(r-l),  0           ],
-            [0,         2*n/(t-b), (t+b)/(t-b),  0           ],
-            [0,         0,         -(f+n)/(f-n), -2*f*n/(f-n)],
-            [0,         0,         -1,           0           ],
-        ]
-    )
+    return np.array([
+        [2*n/(r-l), 0,         (r+l)/(r-l),  0           ],
+        [0,         2*n/(t-b), (t+b)/(t-b),  0           ],
+        [0,         0,         -(f+n)/(f-n), -2*f*n/(f-n)],
+        [0,         0,         -1,           0           ],
+
+    ])
 
 def get_V(bottom_left : tuple, top_right : tuple):
 
     l, b = bottom_left
     r, t = top_right
 
-    return np.array(
-        [
-            [(r-l)/2, 0,       0,   (r+l)/2],
-            [0,       (t-b)/2, 0,   (t+b)/2],
-            [0,       0,       1/2, 1/2    ],
-            [0,       0,       0,   1      ]
-        ]
-    )
+    return np.array([
+        [(r-l)/2, 0,       0,   (r+l)/2],
+        [0,       (t-b)/2, 0,   (t+b)/2],
+        [0,       0,       1/2, 1/2    ],
+        [0,       0,       0,   1      ]
+    ])
 
 class Camera:
 
