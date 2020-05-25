51154be31c9982e4d34eaa102fa37b779484259a
daddabarba <davidebarbieri97@gmail.com>
2020-05-25 13:16:37 +0200

    Added first draft of 2D render

--- code/main.py
+++ code/main.py
@@ -89,6 +89,13 @@ def main(args):
     if args.up_to is not None and args.up_to == "project":
         return
 
+    print("Rendering 2D image")
+
+    print("\tExtracting pixels")
+    face_2D = render(face_uv, face_basis.color, face_basis.mesh.astype(np.int))
+
+    plt.imsave(args.face_2D_file, face_2D)
+
 if __name__ == "__main__":
 
     parser = ArgumentParser()
@@ -190,6 +197,13 @@ if __name__ == "__main__":
         type = str,
         default = "../meshes/face_uv.png",
         help = "File in which to save uv render of face",
+    )
+
+    parser.add_argument(
+        "--face_2D_file",
+        type = str,
+        default = "../meshes/face_2D.png",
+        help = "File in which to save 2D render of face",
     )
 
     main(parser.parse_args())
