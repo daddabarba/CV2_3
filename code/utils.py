724e10a4ac88787dc46c60f32e464807687185a3
daddabarba <davidebarbieri97@gmail.com>
2020-05-26 13:55:50 +0200

    Added im2np method to utils and some comments

--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,25 @@
 import torch
 
+from scipy import misc
+
 def to_homogeneous(x):
+    """
+    Adds homogeneous coordinate to input if needed
+
+    Parameters:
+        x (torch.Tensor) : Nx3 or Nx4 tensor where N is the number of points. If Nx3 a fourth column of ones is added
+    """
 
     if x.shape[1] == 4:
         return x
 
     return torch.cat([x, torch.ones(x.shape[0], 1)], dim = 1)
+
+def im2np(path : str):
+    """
+    Converts a given image to numpy array
+
+    Parameters:
+        path (str) : path to image file
+    """
+    return misc.imread(path)
